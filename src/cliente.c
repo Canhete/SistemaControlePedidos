6d07123781bbebfb953560311d4e26111b0a8259
lays <laysamandamoura@gmail.com>
2025-11-01 14:12:16 -0300

    struct

---
+++ src/cliente.c
@@ -0,0 +1,10 @@
+// estrutura para pessoa física
+struct PessoaFisica{
+    int id;                    // codigo identificador único
+    char cpf[15];              // cpf (formato: 000.000.000-00)
+    char nome[100];            // nome completo
+    char endereco[200];        // endereço completo
+    char telefone[20];         // telefone
+    char celular[20];          // celular
+    char email[100];           // e-mail
+}
