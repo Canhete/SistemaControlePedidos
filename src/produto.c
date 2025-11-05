ddb02ce28560a2136af7af680e5f07a0213247e9
lays <laysamandamoura@gmail.com>
2025-11-05 01:07:14 -0300

    Update produto.c

---
+++ src/produto.c
@@ -0,0 +1,7 @@
+// estrutura para produto
+struct Produto{
+    int codigo;                  // codigo identificador do produto     
+    char descricao[200];         // descrição do produto
+    float preco;                 // preço do produto
+    int estoque;      //quantidade do respectivo produto no inventario
+}
